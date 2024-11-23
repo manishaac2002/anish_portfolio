8679ff90fd0efaea58ebaa23e68c06097ea09bbf
manishaac2002 <manishaac2002@gmail.com>
2024-11-23 15:35:29 +0530

    chore--

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,11 +1,11 @@
 const NavBar = () => {
   return (
     <>
-      <div className=" bg-navyBlue rounded-b-4xl">
+      <div className=" bg-navyBlue min-h-screen rounded-b-4xl">
 
         <div className="flex">
-
-          <div className="flex m-10 box-border bg-glass h-16 w-72 px-3 py-3 rounded-full ">
+            {/* email address */}
+          <div className="flex m-10 box-border bg-glass h-16 w-72 px-3 py-3 rounded-full sm:p-2">
             <p className="text-chalk-100 px-2 py-2">example@email.com</p>
 
             <button className="bg-cloud text-coal ml-2 px-6  rounded-3xl ">
@@ -13,14 +13,15 @@ const NavBar = () => {
             </button>
           </div>
 
-          <div className="flex ml-auto px-10 py-3">
-            <p className="text-chalk-200 font-medium px-8 py-4 mt-8 h-16 rounded-full hover:bg-glass hover:text-cloud">
+            {/* nav bar */}
+          <div className="flex  ml-auto px-10 py-3 ">
+            <p className="text-chalk-200 font-medium px-8 py-4 mt-8 h-16 rounded-full hover:bg-glass hover:text-cloud sm:px-5">
               Services</p>
-            <p className="text-chalk-200 font-medium px-8 py-4 mt-8 h-16 rounded-full hover:bg-glass hover:text-cloud">
+            <p className="text-chalk-200 font-medium px-8 py-4 mt-8 h-16 rounded-full hover:bg-glass hover:text-cloud sm:px-5">
               Research</p>
-            <p className="text-chalk-200 font-medium px-8 py-4 mt-8 h-16 rounded-full hover:bg-glass hover:text-cloud">
+            <p className="text-chalk-200 font-medium px-8 py-4 mt-8 h-16 rounded-full hover:bg-glass hover:text-cloud sm:px-5">
               Project</p>
-            <p className="text-chalk-200 font-medium px-8 py-4 mt-8 h-16 rounded-full hover:bg-glass hover:text-cloud">
+            <p className="text-chalk-200 font-medium px-8 py-4 mt-8 h-16 rounded-full hover:bg-glass hover:text-cloud sm:px-5">
               Blog</p>
 
           </div >
@@ -28,11 +29,11 @@ const NavBar = () => {
         </div>
 
         <div className="mx-auto my-24 p-4 ">
-          
+          {/* image */}
           <div className="w-72 h-72 rounded-full mx-auto ">
             <img className="" src="./AnishBalaSachin.webp" alt="" />
           </div>
-          
+          {/* about content */}
           <div className="text-center ">
             <p className="text-chalk-200 text-4xl flex justify-center text-center">
               Transforming ideas into impactful<br />
@@ -53,5 +54,6 @@ const NavBar = () => {
 }
 
 export default NavBar
+
 
 
