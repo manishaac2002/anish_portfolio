6274053554c015efefcda8dd015b79071f924577
manishaac2002 <manishaac2002@gmail.com>
2024-11-30 16:08:04 +0530

    feat : started a service page .

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -6,12 +6,12 @@ const Company = () => {
     <div className="bg-navyBlue">
 
     <div className="whiteContainerSm font-sans">
-        <div className="mt-16 md:mt-24 text-center">
+        <div className=" text-center">
             {/* company quote */}
-            <p className="text-chalk-400 text-2xl md:text-3xl ">From Code to Collaboration</p>
+            <p className="text-chalk-400 text-2xl md:text-3xl mt-16 md:mt-24">From Code to Collaboration</p>
             <p className="text-coal text-5xl md:text-8xl mt-5">My Career Story</p>
         </div>
-        <div className="flex flex-wrap justify-center mt-12 md:mt-24 sm:grid-cols-2 sm:gap-12 md:grid-cols-4 md:gap-16 lg:gap-56">
+        <div className="flex justify-center mt-12 md:mt-24 sm:grid-cols-2 sm:gap-12 md:grid-cols-4 md:gap-16 lg:gap-56">
             {/* company image */}
             <img className=" hidden md:block md:px-14 lg:px-20 xl:px-48" src={logo} alt="logo" />
             <img className="md:hidden px-8 " src={logoSm} alt="logo" />
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,12 +1,23 @@
 const Services = () => {
   return (
     <div className="blueContainer ">
-        <div>
-            {/* title */}
+
+        <div className=" text-center">
+          {/* company quote */}
+          <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">Empathy is the foundation of exceptional</p>
+          <p className="text-chalk-100 text-5xl md:text-8xl pt-6">Services</p>
+        </div >
+        <div className="border-b-2 border-chalk-200 w-3/4 mx-auto pb-20 ">
+
         </div>
-        <div>
-            {/* content */}
-        </div>
+      
+      <div className="flex justify-center gird-cols-1 md:grid-cols-2 gap-96">
+        {/* content */}
+        <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">test</p>
+        <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">test</p>
+        <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">test</p>
+        <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">test</p>
+      </div>
     </div>
   )
 }
