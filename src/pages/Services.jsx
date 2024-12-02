15140792fbc3775dd4a1f03e015ce22a49b379ce
manishaac2002 <manishaac2002@gmail.com>
2024-12-02 13:52:43 +0530

    chore--

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,24 +2,49 @@ const Services = () => {
   return (
     <div className="blueContainer ">
 
-        <div className=" text-center">
-          {/* company quote */}
-          <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">Empathy is the foundation of exceptional</p>
-          <p className="text-chalk-100 text-5xl md:text-8xl pt-6">Services</p>
-        </div >
-        <div className="border-b-2 border-chalk-200 w-3/4 mx-auto pb-20 ">
+      <div className=" text-center">
+        {/* company quote */}
+        <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">Empathy is the foundation of exceptional</p>
+        <p className="text-chalk-100 text-5xl md:text-8xl pt-6">Services</p>
+      </div >
+      <div className="border-b-2 border-chalk-200 w-3/4 mx-auto pb-20 ">
 
-        </div>
-      
+      </div>
+
       <div className="flex justify-center gird-cols-1 md:grid-cols-2 gap-96">
         {/* content */}
-        <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">test</p>
-        <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">test</p>
-        <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">test</p>
-        <p className="text-chalk-200 text-2xl md:text-3xl pt-16 ">test</p>
+        <div className="text-chalk-200 text-2xl md:text-3xl pt-16 ">
+          <p>Research</p>
+        </div>
+        <div className="text-chalk-200 text-2xl md:text-3xl pt-16 ">
+          <p>Product design</p>
+        </div>
+        <div className="text-chalk-200 text-2xl md:text-3xl pt-16 ">
+          <p>UI/UX</p>
+        </div>
+        <div className="text-chalk-200 text-2xl md:text-3xl pt-16 ">
+          <p>Development</p>
+        </div>
       </div>
     </div>
   )
 }
 
 export default Services
+
+{/* <svg width="55" height="50" viewBox="0 0 55 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M43.8652 27.1504C43.8652 28.4839 44.9462 29.5648 46.2796 29.5648C47.613 29.5648 48.6939 28.4839 48.6939 27.1504C48.6939 25.817 47.613 24.7361 46.2796 24.7361" stroke="#E9E9E9" stroke-width="1.25"/>
+<path d="M2.58008 40.2957V29.6393C2.58008 29.0875 3.02737 28.6402 3.57912 28.6402H8.24132M25.8911 40.2957V29.6393C25.8911 29.0875 25.4438 28.6402 24.892 28.6402H20.2298M8.24132 28.6402L11.2385 15.9857M8.24132 28.6402H20.2298M11.2385 15.9857V13.6563C11.2385 12.8547 11.7377 12.138 12.4895 11.8601L15.5676 10.7226M11.2385 15.9857L11.9133 16.6169C13.054 17.6841 14.7459 17.9104 16.1271 17.1807L18.0705 16.1539M20.2298 28.6402L18.0705 16.1539M18.0705 16.1539L41.8758 6.75361M18.0705 16.1539L18.3671 15.6594C19.4414 13.869 18.5171 11.5472 16.5062 10.985L15.5676 10.7226M41.8758 6.75361L44.9362 9.11703C45.1111 9.2521 45.3258 9.32536 45.5468 9.32536H46.7045M41.8758 6.75361V1M41.8758 6.75361L45.2059 3.97334M41.8758 1L15.5676 10.7226M41.8758 1H42.6137C43.1863 1 43.7141 1.30939 43.9939 1.80894L45.2059 3.97334M45.2059 3.97334L46.7045 6.64935L47.4709 8.01785C47.7987 8.60334 47.3755 9.32536 46.7045 9.32536V9.32536M42.8748 32.3034L46.1566 30.2854C46.4903 30.0802 46.9131 30.089 47.238 30.3078L50.2012 32.3034M42.8748 32.3034V36.9656C42.8748 37.5173 43.3221 37.9646 43.8739 37.9646H49.2021C49.7539 37.9646 50.2012 37.5173 50.2012 36.9656V32.3034M42.8748 32.3034H46.7045H50.2012M46.7045 9.32536V25.3933" stroke="#E9E9E9" stroke-width="1.25"/>
+<path d="M44.791 8.74271L47.1221 7.07764" stroke="#E9E9E9" stroke-width="1.25"/>
+<rect x="0.625" y="42.1694" width="26.3902" height="6.32608" rx="3.16304" stroke="#E9E9E9" stroke-width="1.25"/>
+<mask id="path-5-inside-1_277_2" fill="white">
+<rect x="36.5488" y="41.1284" width="18.2325" height="8.32536" rx="0.972442"/>
+</mask>
+<rect x="36.5488" y="41.1284" width="18.2325" height="8.32536" rx="0.972442" stroke="#E9E9E9" stroke-width="2.5" mask="url(#path-5-inside-1_277_2)"/>
+<path d="M18.4824 16.0689L21.6789 9.67583C21.9333 9.16709 22.5605 8.97306 23.0577 9.24929L27.3068 11.6099C27.7731 11.869 28.3607 11.716 28.6414 11.2624L31.8809 6.02942C32.1945 5.5228 32.8791 5.40158 33.3476 5.76971L36.321 8.10593C36.7644 8.4543 37.408 8.36694 37.7425 7.91299L41.7934 2.41528" stroke="#E9E9E9" stroke-width="1.25"/>
+<path d="M16.8184 10.4076L22.6874 13.0487C23.1412 13.2529 23.6762 13.0919 23.942 12.6711L26.9705 7.87595C27.2535 7.42789 27.8371 7.27875 28.3004 7.53611L32.589 9.91866C33.0739 10.1881 33.6855 10.0108 33.9512 9.52374L36.8519 4.20588C37.1633 3.6349 37.9286 3.50902 38.4065 3.95017L41.7944 7.07747" stroke="#E9E9E9" stroke-width="1.25"/>
+<circle cx="3.49685" cy="45.2908" r="1.2488" fill="#E9E9E9"/>
+<circle cx="24.1433" cy="45.2908" r="1.2488" fill="#E9E9E9"/>
+<circle cx="16.9832" cy="45.2908" r="1.2488" fill="#E9E9E9"/>
+<circle cx="9.40701" cy="45.2908" r="1.2488" fill="#E9E9E9"/>
+</svg> */}
