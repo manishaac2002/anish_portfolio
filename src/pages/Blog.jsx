6dbfd9ec22bffd5f8e12b675c6c5b3c14ae43a6c
manishaac2002 <manishaac2002@gmail.com>
2024-12-07 12:05:51 +0530

    feat-completed blog page || style : responsive is still pending

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,3 @@
-// import iconBlog from '../images/IconBlog.webp'
-// import MousePointerBlog from "../images/MousePointerBlog.webp";
-// import TouchScreenBlog from "../images/TouchScreenBlog.webp";
 const Blog = () => {
     return (
         <div className="bg-navyBlue">
@@ -14,29 +11,16 @@ const Blog = () => {
                 <div className=" grid grid-cols-3 pt-20 mx-12 ">
                     {/* blog posts */}
                     <div className=''>
-                        {/* <img className='shadow-lg rounded-2xl ' src={iconBlog} alt="iconBlog" />
-                        <p className='text-sm md:text-lg xl:text-2xl font-semibold pt-8'>
-                            The Hamburger Menu </p>
-                        <p className='text-justify text-xs md:text-xl pt-3'>
-                            The hamburger menu, designed by Norm Cox in 1981, is a timeless icon that saves space and simplifies navigation.</p> */}
-                            <iframe className='shadow-lg rounded-2xl ml-8' src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7248290668337373184" height="720" width="504"  allowfullscreen="" title="Embedded post"></iframe>
+                        <iframe className='shadow-lg rounded-2xl ml-8' src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7248290668337373184" 
+                        height="720" width="504" allowfullscreen="" title="Embedded post"></iframe>
                     </div>
                     <div>
-                        {/* <img className='shadow-lg rounded-2xl ' src={MousePointerBlog} alt="iconBlog" />
-                        <p className='text-sm md:text-lg xl:text-2xl font-semibold pt-8'>
-                            Why the Mouse Pointer is Tilted </p>
-                        <p className='text-justify text-xs md:text-xl pt-3'>
-                            The tilted mouse pointer, designed at Xerox PARC for visibility on low-res screens, remains a timeless and universal standard.</p> */}
-                            <iframe className='shadow-lg rounded-2xl ml-8' src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7234065982300807168" height="716" width="504" allowfullscreen="" title="Embedded post"></iframe>
+                        <iframe className='shadow-lg rounded-2xl ml-8' src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7234065982300807168" 
+                        height="716" width="504" allowfullscreen="" title="Embedded post"></iframe>
                     </div>
                     <div>
-                        {/* <img className='shadow-lg rounded-2xl' src={TouchScreenBlog} alt="iconBlog" />
-                        <p className='text-sm md:text-lg xl:text-2xl font-semibold pt-6'>
-                             Touchscreen Interfaces </p>
-                        <p className='text-justify text-xs md:text-xl pt-2'>
-                            Optimized touchscreen interfaces use large targets, clear visuals, and consistent, accessible designs to enhance usability and
-                            reduce errors in fields like healthcare.</p> */}
-                            <iframe className='shadow-lg rounded-2xl ml-8' src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7251841149294817280" height="725" width="504" allowfullscreen="" title="Embedded post"></iframe>
+                        <iframe className='shadow-lg rounded-2xl ml-8' src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7251841149294817280" 
+                        height="725" width="504" allowfullscreen="" title="Embedded post"></iframe>
                     </div>
 
                 </div>
