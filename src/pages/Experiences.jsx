c3b2dc0302fadf70464ac8a2d7932f730f345b87
manishaac2002 <manishaac2002@gmail.com>
2024-12-17 15:56:02 +0530

    feat - modified the experience page title.

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -3,11 +3,18 @@
 const Experiences = () => {
   return (
     <div className="blueContainerSm">
-        <p className="text-chalk-200 text-4xl flex justify-center pt-16 font-semibold">Experiences</p>
-        <div className="grid grid-cols-2 text-center text-cloud text-2xl md:text-4xl lg:text-6xl font-bold pt-14 md:pt-24  ">
-            <p>5+ years</p>
-            <p>4 companies</p>
-        </div>
+      {/* <p className="text-chalk-200 text-4xl flex justify-center pt-16 font-semibold">Experiences</p> */}
+      <div className=" text-center pt-12">
+      <span className="text-chalk-100 font-semibold text-4xl md:text-6xl">Experiences
+      </span>
+        <p className="text-chalk-200 text-xl md:text-3xl pt-3 ">
+           that Inspire, Challenge, and Transform
+        </p>
+      </div>
+      <div className="grid grid-cols-2 text-center text-cloud text-2xl md:text-4xl lg:text-6xl font-bold pt-14 md:pt-24  ">
+        <p>5+ years</p>
+        <p>4 companies</p>
+      </div>
     </div>
   )
 }
